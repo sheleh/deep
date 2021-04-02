12f02b8de379063ff86014bb5aa1776639641849
sheleh <shelex87@gmail.com>
2021-04-02 14:04:20 +0300

    pre Alpha

--- covid_prediction_prj/DB/prediction_data_db_engine.py
+++ covid_prediction_prj/DB/prediction_data_db_engine.py
@@ -1,8 +1,6 @@
-from sqlalchemy import create_engine, Column, String, Integer, Date, inspect
+from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from covid_prediction_prj.parse import concatenated_pd
-from sqlalchemy.sql import text
 import pandas as pd
 
 engine = create_engine('sqlite:///DB/prediction_data.db', echo=False)
@@ -13,21 +11,21 @@ session = Session()
 conn = engine.connect()
 
 
-def save_prediction_to_db(country, data):
+def save_prediction_to_db(country, data, type_of):
     with engine.begin() as connection:
         print(f'ILL SAVE CALCULATIONS to DB')
-        data.to_sql(country + '_calculation_data', con=connection, if_exists='replace', index=True)
+        data.to_sql(country + '_calculation_data_' + type_of, con=connection, if_exists='replace', index=True)
 
 
-def read_pd_prediction_from_sql(country):
+def read_pd_prediction_from_sql(country, type_of):
     with engine.begin() as connection:
-        df = pd.read_sql_table(country + '_calculation_data', connection)
+        df = pd.read_sql_table(country + '_calculation_data_' + type_of, connection)
         print('ILL READ CALCULATIONS FROM DB')
     return df
 
 
-def tablename_check_prediction(country):
+def tablename_check_prediction(country, type_of):
     with engine.begin() as connection:
         inspection = inspect(connection)
-        return inspection.has_table(country + '_calculation_data')
-
+        res = inspection.has_table(country + '_calculation_data_' + type_of)
+    return res
